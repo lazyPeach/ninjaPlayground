3599dee10ae452d611bc61191c21973323d79dd5
lorenzo_patras <lorenzo.patras@nuance.com>
2015-01-21 14:04:43 +0200

    Unmanaged C dll used in C#

---
+++ unmanagedDll/CSTestApp/CSTestApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSTestApp
+{
+  class Program
+  {
+    [DllImport("F:\\gitHubStuff\\microsoftStuff\\unmanagedDll\\CppDll\\Debug\\CppDll.dll", CallingConvention = CallingConvention.Cdecl)]
+    public static extern double Add(double a, double b);
+
+    static void Main(string[] args)
+    {
+      double c = Add(2.3, 4.5);
+      Console.WriteLine("Test");
+      Console.ReadLine();
+    }
+  }
+}
